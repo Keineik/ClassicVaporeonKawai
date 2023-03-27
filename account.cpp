fa55262c3448b9915f3deb4e0d295808c387b0b5
Văn Bá Đức Kiên <61812509+Keineik@users.noreply.github.com>
2023-03-27 23:00:24 +0700

    account and files manipulation (in progress)

---
+++ account.cpp
@@ -0,0 +1,215 @@
+#include "account.h"
+
+void readBinFile() {
+    fstream fs;
+    fs.open("savefile.bin", ios::in | ios::binary);
+
+    if (!fs.is_open()) {
+        cout << "File invalid";
+    }
+    else {
+        fs.seekg(0, ios::end);
+        int file_size = (int) fs.tellg();
+        fs.seekg(0, ios::beg);
+
+        while(fs.tellg() < file_size) {
+            savefile tempsf;
+            char temp[50];
+
+            fs.read(&tempsf.mask, 1);
+
+            fs.read(temp, NAMESIZE);
+            xorCstr(tempsf.name, temp, tempsf.mask, NAMESIZE);
+
+            fs.read(temp, PASSSIZE);
+            xorCstr(tempsf.password, temp, tempsf.mask, PASSSIZE);
+
+            for (int i = 0; i < 5; i++)
+                fs.read((char*)&tempsf.record[i], sizeof(Record));
+
+            for (int i = 0; i < 5; i++)
+                fs.read((char*)&tempsf.state[i], sizeof(State));
+
+            saves.push_back(tempsf);
+            listOfUsername.insert(tempsf.name);
+        }
+    }
+
+    fs.close();
+}
+
+void xorCstr(char* dcstr, char* cstr, char mask, int size) {
+    for (int i = 0; i < size; i++) {
+        dcstr[i] = cstr[i] ^ mask;
+    }
+}
+
+void writeBinFile() {
+    fstream fs;
+    fs.open("savefile.bin", ios::out | ios::binary);
+
+    for (auto tempsf: saves) {
+        char temp[50];
+        fs.write(&tempsf.mask, 1);
+
+        xorCstr(temp, tempsf.name, tempsf.mask, NAMESIZE);
+        fs.write(temp, NAMESIZE);
+
+        xorCstr(temp, tempsf.password, tempsf.mask, PASSSIZE);
+        fs.write(temp, PASSSIZE);
+
+        for (int i = 0; i < 5; i++)
+            fs.write((char*)&tempsf.record[i], sizeof(Record));
+
+        for (int i = 0; i < 5; i++)
+            fs.write((char*)&tempsf.state[i], sizeof(State));
+        
+        fs.close();
+    }
+}
+
+int signUp(char* username, char* password, char* retypePassword) {
+    if (listOfUsername.count(username))
+        return -1; // username exsisted
+    else if (username[0] == '\0')
+        return -2; // username invalid
+    else if (strcmp(password, retypePassword) != 0) {
+        return 0; // password and retypePassword do not match
+    }
+    else {
+        currentSave.mask = 's';
+        strcpy(currentSave.name, username);
+        strcpy(currentSave.password, password);
+        for (int i = 0; i < 5; i++) {
+            currentSave.record[i] = {0, 0, 0, 0};
+            currentSave.state[i] = {0, 0, 1, 1};
+        }
+        
+        listOfUsername.insert(username);
+        saves.push_back(currentSave);
+        return 1;
+    }
+}
+
+bool login(char* username, char* password) {
+    if (listOfUsername.count(username) == 0)
+        return false;
+    for (auto user: saves) {
+        if (strcmp(username, user.name) == 0) {
+            if (strcmp(password, user.password) != 0)
+                return false;
+            else {
+                currentSave.mask = user.mask;
+                strcpy(currentSave.name, user.name);
+                strcpy(currentSave.password, user.password);
+                for (int i = 0; i < 5; i++) {
+                    currentSave.record[i] = user.record[i];
+                    currentSave.state[i].p = user.state[i].p;
+                    currentSave.state[i].q = user.state[i].q;
+                    currentSave.state[i].p_ = user.state[i].p_;
+                    currentSave.state[i].q_ = user.state[i].q_;
+                    strcpy(currentSave.state[i].board, user.state[i].board);
+                    strcpy(currentSave.state[i].file_background, user.state[i].file_background);
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void logout() {
+    currentSave.mask = 's';
+    currentSave.name[0] = '\0';
+    currentSave.password[0] = '\0';
+    for (int i = 0; i < 5; i++) {
+        currentSave.record[i] = {0, 0, 0, 0};
+        currentSave.state[i] = {0, 0, 1, 1};
+        currentSave.state[i].board[0] = '\0';
+        currentSave.state[i].file_background[0] = '\0';
+    }
+}
+
+int main () {
+    currentSave.mask = 's';
+    strcpy(currentSave.name, "Bocchi The Rock");
+    strcpy(currentSave.password, "UoghhhSegsss");
+    for (int i = 0; i < 5; i++) {
+        currentSave.record[i] = {25, 3, 2023, i*100};
+        currentSave.state[i] = {0, 0, 1, 1};
+    }
+
+    char username[50], password[50], retypePassword[50];
+    int type = 1;
+    while (type != 0) {
+        int type;
+        cout << "Sign Up or Login: "; cin >> type;
+        cin.ignore();
+        if (type == 1) {
+            cout << "Type username: "; cin.getline(username, 50);
+            cout << "Type password: "; cin.getline(password, 50);
+            cout << "Confirm password: "; cin.getline(retypePassword, 50);
+            int signUpStatus = signUp(username, password, retypePassword);
+            if (signUpStatus == 1) 
+                cout << "Sign Up successfully" << endl;
+            else if (signUpStatus == -1)
+                cout << "Username already exsisted" << endl;
+            else if (signUpStatus == -2)
+                cout << "Username is not valid" << endl;
+            else {
+                cout << "Password do not match" << endl;
+            }
+        }
+        else if (type == 2) {
+            cout << "Type username: "; cin.getline(username, 50);
+            cout << "Type password: "; cin.getline(password, 50);
+            if (login(username, password)) {
+                cout << "Login successful" << endl;
+            }
+            else {
+                cout << "Login failed" << endl;
+            }
+        }
+        else if (type == 3) {
+            logout();
+        }
+        else if (type == 4) {
+            cout << "User info: " << endl;
+            cout << "Mask: " << currentSave.mask << endl;
+            cout << "Username: " << currentSave.name << endl;
+            cout << "Password: " << currentSave.password << endl;
+
+            for (int i = 0; i < 5; i++) {
+                auto rec = currentSave.record[i];
+                cout << "Record " << i + 1 << ": ";
+                cout << rec.date.dd << "/" << rec.date.mm << "/" << rec.date.yy << ": " << rec.points << endl;
+            }
+
+            for (int i = 0; i < 5; i++) {
+                auto st = currentSave.state[i];
+                cout << "Board size: " << st.p << " " << st.q << endl;
+                cout << "Current position: " << st.p_ << " " << st.q_ << endl;
+            }
+            cout << endl;
+        }
+    }
+    
+    for (auto sf: saves) {
+        cout << "Mask: " << sf.mask << endl;
+        cout << "Username: " << sf.name << endl;
+        cout << "Password: " << sf.password << endl;
+
+        for (int i = 0; i < 5; i++) {
+            auto rec = sf.record[i];
+            cout << "Record " << i + 1 << ": ";
+            cout << rec.date.dd << "/" << rec.date.mm << "/" << rec.date.yy << ": " << rec.points << endl;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            auto st = sf.state[i];
+            cout << "Board size: " << st.p << " " << st.q << endl;
+            cout << "Current position: " << st.p_ << " " << st.q_ << endl;
+        }
+        cout << endl;
+    }
+}
---
+++ account.h
@@ -0,0 +1,56 @@
+#include "header.h"
+
+#define PADDING 500 // bytes
+#define NAMESIZE 50
+#define PASSSIZE 50
+#define BOARDSIZE 999
+#define URLSIZE 100
+
+struct State{ //Representing a board state
+    int p, q; // Size of the board game
+    int p_, q_; // Current cursor position
+    char board[BOARDSIZE]; // Current board state
+    char file_background[URLSIZE]; // Link to background file. This variable’s value is NULL if there is no current background
+    // 500 byte NULL
+};
+struct Date{
+    int dd, mm, yy;
+};
+struct Record{
+    Date date; // Date of completed record
+    int points; // points achieved
+    // 500 byte NULL
+};
+struct savefile{
+    char mask; // You are required to transfer all char-type variables by performing xor each with the mask-variable, bit-by-bit.
+    char name[NAMESIZE]; // username
+    char password[PASSSIZE]; // password
+    // 500 byte NULL
+    Record record[5]; // List of sorted best records
+    State state[5]; // List of save state
+};
+unordered_set<string> listOfUsername;
+savefile currentSave;
+vector<savefile> saves;
+
+// LEADERBOARD  
+struct topPlayer {
+    char name[NAMESIZE];
+    int points;
+};
+vector<topPlayer> leaderboard;
+int top5score = -1;
+
+// BINARY FILE INTERACTIONS
+void readBinFile();
+// xor cstr with mask and asign that to dcstr
+void xorCstr(char* dcstr, char* cstr, char mask, int size);
+void writeBinFile();
+
+// ACCOUNT INTERACTIONS
+// Sign Up: 1 -> succesful, 0 -> password don't match, -1 -> username exsisted, -2 -> username invalid 
+int signUp(char* username, char* password, char* retypePassword);
+bool login(char* username, char* password);
+void logout();
+void saveGame(int saveSlot);
+bool loadGame(int saveSlot);
